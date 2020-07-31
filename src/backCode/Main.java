5578de5a6eb90328456150a6cb14c1a7232d86b0
Alireza Ghafari <areza.ghafari01@gmail.com>
2020-07-31 18:38:20 +0430

    Add first commit

---
+++ src/backCode/Main.java
@@ -0,0 +1,44 @@
+package backCode; /*** In The Name of Allah ***/
+
+import graphic.GameFrame;
+import graphic.GameLoop;
+import graphic.ThreadPool;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Program start.
+ *
+ * @author Seyed Mohammad Ghaffarian
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialize the global thread-pool
+        ThreadPool.init();
+
+
+        // Show the game menu ...
+//        new MenuFrame();
+
+        // After the player clicks 'PLAY' ...
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GameFrame frame = new GameFrame("Simple Ball !");
+                frame.setLocationRelativeTo(null); // put frame at center of screen
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+
+                frame.initBufferStrategy();
+                // Create and execute the game-loop
+                GameLoop game = new GameLoop(frame);
+                game.init();
+                ThreadPool.execute(game);
+                // and the game starts ...
+            }
+        });
+
+    }
+}
