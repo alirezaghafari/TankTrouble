70c73367f5c87c0010984fc570ca48e1e785be6a
Alireza Ghafari <srezaee092@gmail.com>
2020-08-06 01:12:44 +0430

    Add bullet to project

---
+++ src/Bullet.java
@@ -0,0 +1,32 @@
+package project;
+
+public class Bullet {
+
+	private int timer;
+	private int owner;
+	private int angle;
+	private Point position;
+	private static final double bulletSpeed = 1.5;
+
+	private GameEngine engine;
+
+	public Bullet(int player, Point position, int angle, GameEngine e) {
+		this.owner = player;
+		this.angle = angle;
+		this.position = position;
+		this.timer = 1000; //set the time duration of the bullet's presence to 100 frames
+		this.engine = e;
+	}
+
+	public Point getPosition() {
+		return position;
+	}
+
+	public int getAngle() {
+		return angle;
+	}
+
+}
+
+
+
