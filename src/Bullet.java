62ebe2c3b64be314580786a0ab01b6f4d3987b7a
Alireza Ghafari <srezaee092@gmail.com>
2020-08-06 02:09:46 +0430

    Add collision checker to bullent

--- src/Bullet.java
+++ src/Bullet.java
@@ -65,6 +65,73 @@ public class Bullet{
 		}
 		this.position=nextPoint;
 	}
+
+	public boolean reduceTimer(){
+		//count down the timer.  If time is up, remove the bullet.  Return whether the bullet was removed
+		timer--;
+		if (timer<0){
+			this.removeBullet();
+			return true;
+		}
+		return false;
+	}
+
+	private void flipBulletV(){
+		this.angle=(-this.angle) + 360;
+	}
+
+	private void flipBulletH(){
+		if (this.angle>180){
+			this.angle = -this.angle+540;
+		}else{
+			this.angle=-this.angle+180;
+		}
+	}
+
+	public void tankCollision(){
+		//checks whether the bullet has collided with player1 or player2, calling the appropriate functions if so
+		if (collision(engine.player1)){
+			this.removeBullet();
+			engine.player1.hit();
+		}else if (collision(engine.player2)){
+			this.removeBullet();
+			engine.player2.hit();
+		}
+
+	}
+
+	private boolean collision(Player player){
+		//work out the distance between the centre of the bullet and the player
+		double distance = Point.distance(player.getCoordinates(),this.position);
+		//return whether they're overlapping
+		return (distance<=(GameEngine.tankWidth/2+GameEngine.bulletWidth/2));
+	}
+	public Point(double a, double b){
+		this.setxCoord(a);
+		this.setyCoord(b);
+	}
+
+	//Getter and Setter methods:
+	public double getxCoord() {
+		return xCoord;
+	}
+
+	public void setxCoord(double xCoord) {
+		this.xCoord = xCoord;
+	}
+
+	public double getyCoord() {
+		return yCoord;
+	}
+
+	public void setyCoord(double yCoord) {
+		this.yCoord = yCoord;
+	}
+
+	public void setCoords(double xCoord, double yCoord){
+		this.xCoord=xCoord;
+		this.yCoord=yCoord;
+	}
 }
 
 
