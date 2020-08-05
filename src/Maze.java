d1c27465f2f8ed21e4aab96025aeae3d6127b4e9
Alireza Ghafari <srezaee092@gmail.com>
2020-08-06 04:11:41 +0430

    Add neighbours checker and given to maze class

--- src/Maze.java
+++ src/Maze.java
@@ -106,4 +106,126 @@ public class Maze {
 		}
 		//remove nulls}
 	}
+	private static boolean isNeighbour(Square a, Square b){
+		//takes two squares and says whether or not they are direct neighbours
+		int x = a.getxCoord();
+		int y = a.getyCoord();
+		int x2 = b.getxCoord();
+		int y2 = b.getyCoord();
+		return (x2-1==x && y2==y)||(x2+1==x && y2==y)||(x2==x && y2-1==y)||(x2==x && y2+1==y);
+	}
+	private void removeWall(Square[] area){
+		// when given an array of squares that make up an area, remove a random wall (or two) between that area and another
+		// ie connect that area to a random other one (or two)
+		Square[] neighbours = areaNeighbours(area);
+		//find all neighbours of the area
+		int numberToRemove=2;
+		//may or may not want to remove multiple walls to connect to multiple areas?
+		for (int j = 0; j<numberToRemove;j++){
+
+
+			int randomNum = (int) (Math.random()*neighbours.length);
+			Square chosen = neighbours[randomNum];
+			//choose a random square from the list of neighbours
+			Square areaSquareChosen = new Square(0,0);
+			for (int i = 0; i<area.length; i++){
+				if (isNeighbour(area[i],chosen)){
+					areaSquareChosen=area[i];
+					break;
+				}
+			}
+			//find a square in the area adjacent to that one
+			int x = chosen.getxCoord();
+			int y = chosen.getyCoord();
+			int x2 = areaSquareChosen.getxCoord();
+			int y2 = areaSquareChosen.getyCoord();
+			if (x==x2){
+				walls[0][Math.max(y,y2)][x]=false;
+			}else{
+				walls[1][Math.max(x2, x)][y]=false;
+			}
+		}
+		//remove the wall between those two squares
+	}
+	private Square[] areaNeighbours(Square[] area){
+		//when given an array of squares that make up an area, returns all neighbours of that area
+		//doesn't return any squares in the area itself
+		Square[] neighbours = new Square[49];
+		//return all walled neighbours{
+		for (int i = 0; i<area.length;i++){
+			neighbours =  merge(neighbours,walledNeighbours(area[i]));
+		}
+		//return all walled neighbours}
+		//remove any squares found that are actually in the area itself{
+		Square[] cutNeighbours = new Square[neighbours.length];
+		int cutNeighboursLength=0;
+		for (int i = 0;i<neighbours.length;i++){
+			boolean found = false;
+			for (int j=0;j<area.length;j++){
+				if (neighbours[i].equals(area[j])){
+					found = true;
+				}
+			}
+			if (!found){
+				cutNeighbours[cutNeighboursLength]=neighbours[i];
+				cutNeighboursLength++;
+			}
+		}
+		//remove any squares found that are actually in the area itself}
+		//remove nulls{
+		Square[] returnValue = new Square[cutNeighboursLength];
+		for (int i = 0; i<cutNeighboursLength;i++){
+			returnValue[i]=cutNeighbours[i];
+		}
+		//remove nulls}
+		return returnValue;
+	}
+	private Square[] walledNeighbours(Square square){
+		//returns all neighbour squares of the input square which are separated by a wall
+		Square[] neighbours = new Square[4];
+		int numberOfNeighbours = 0;
+		//find all walled neighbours{
+		if (isWallLeft(square.getxCoord(), square.getyCoord())&&square.getxCoord()-1>=0) {
+			neighbours[numberOfNeighbours] = new Square(square.getxCoord() - 1,
+					square.getyCoord());
+			numberOfNeighbours++;
+		}
+		if (isWallRight(square.getxCoord(), square.getyCoord())&&square.getxCoord()+1<7) {
+			neighbours[numberOfNeighbours] = new Square(square.getxCoord() + 1,
+					square.getyCoord());
+			numberOfNeighbours++;
+		}
+		if (isWallAbove(square.getxCoord(), square.getyCoord())&&square.getyCoord()-1>=0) {
+			neighbours[numberOfNeighbours] = new Square(square.getxCoord(),
+					square.getyCoord() - 1);
+			numberOfNeighbours++;
+		}
+		if (isWallBelow(square.getxCoord(), square.getyCoord())&&square.getyCoord()+1<7) {
+			neighbours[numberOfNeighbours] = new Square(square.getxCoord(),
+					square.getyCoord() + 1);
+			numberOfNeighbours++;
+		}
+		//find all walled neighbours}
+		// remove nulls{
+		int realLength = 0;
+		boolean found = false;
+		for (int i = 0; i < neighbours.length; i++) {
+			if (neighbours[i] == null) {
+				realLength = i;
+				found = true;
+				break;
+			}
+		}
+		if (found) {
+			Square[] cutReturnValue = new Square[realLength];
+			for (int i = 0; i < realLength; i++) {
+				cutReturnValue[i] = neighbours[i];
+			}
+			return cutReturnValue;
+		} else {
+			return neighbours;
+		}
+		//remove nulls}
+	}
+
 }
